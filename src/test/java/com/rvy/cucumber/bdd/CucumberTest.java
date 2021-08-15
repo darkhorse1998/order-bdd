484777c42d20e0404bc495fd20245e34360ca855
Hrishikesh Kalita <hrishikeshkalita11@gmail.com>
2021-08-15 11:57:08 +0530

    edited1

--- src/test/java/com/rvy/cucumber/bdd/CucumberTest.java
+++ src/test/java/com/rvy/cucumber/bdd/CucumberTest.java
@@ -19,7 +19,7 @@ import cucumber.api.junit.Cucumber;
 //		monochrome = true
 //	)
 @CucumberOptions(
-		features = "src/test/resources/features",
+		features = "sr	test/resources/features",
 		glue= "com.rvy.cucumber.bdd.stepdefs",
 		plugin = { "pretty", "json:target/Cucumber.json" },
 		monochrome = true
